a8c65d61b6d975c4fffa36fec194348e2d31a1b1
skunbydev <skunbydev@gmail.com>
2023-06-28 18:02:27 -0300

    adiciao novo exer

---
+++ JavaScript Exercícios/Exercicios-JavaScript/Objetos/script/script.js
@@ -0,0 +1,31 @@
+// Crie um objeto com os seus dados pessoais
+// Deve possui pelo menos duas propriedades nome e sobrenome
+
+var pessoa = {
+    nome: 'Luiz Felipe',
+    sobrenome : 'Santos de Lima Alves',
+    idade: 20 +  'anos',
+    possuiFaculdade: true, 
+};
+ 
+possui 
+
+
+
+
+// Crie um método no objeto anterior, que mostre o seu nome completo
+
+var nomeCompleto = pessoa.nome + ' ' + pessoa.sobrenome;
+console.log (nomeCompleto);
+
+// Modifique o valor da propriedade preco para 3000
+var carro = {
+    preco: 3000,
+    portas: 4,
+    marca: 'Audi',
+  }
+  
+  // Crie um objeto de um cachorro que represente um labrador,
+  // preto com 10 anos, que late ao ver um homem
+  
+  
