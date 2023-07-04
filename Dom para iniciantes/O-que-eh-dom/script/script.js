b54f674e7904e33e90910b703a1f584e18692243
skunbydev <skunbydev@gmail.com>
2023-07-03 23:09:55 -0300

    adicao novo exercicio

--- Dom para iniciantes/O-que-eh-dom/script/script.js
+++ Dom para iniciantes/O-que-eh-dom/script/script.js
@@ -1,8 +1,20 @@
 // Retorne o url da página atual utilizando o objeto window
 
+const retornaUrl = window.location.href;
+console.log(retornaUrl);
+
 // Seleciona o primeiro elemento da página que
 // possua a classe ativo
 
+const classAtiva = document.querySelector('.ativa');
+console.log(classAtiva);
+
 // Retorne a linguagem do navegador
+const retornaLingua = navigator.language;
+
+console.log(retornaLingua);
 
 // Retorne a largura da janela 
+
+const larguraJanela = window.innerWidth;
+console.log(larguraJanela);
