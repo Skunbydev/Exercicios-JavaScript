27ae6d98e34b75f71ad8dce214731a5ce060c319
skunbydev <skunbydev@gmail.com>
2023-07-07 17:01:10 -0300

    novos exercicios

--- Dom para iniciantes/Eventos/script/script.js
+++ Dom para iniciantes/Eventos/script/script.js
@@ -3,13 +3,33 @@
 // demais itens caso eles possuam a mesma. Previna
 // o comportamento padrão desses links
 
+const linkExternos = document.querySelector('a[href^="https"]');
+console.log(linkExternos);
+
+function handleNoLink(event) {
+    event.preventDefault();
+}
+function handleAtivaClass(event) {
+    if (linkExternos.className === 'ativo') {
+        linkExternos.classList.remove('ativo');
+    } else {
+        linkExternos.classList.add('ativo');
+    }
+}
+linkExternos.addEventListener('click', handleAtivaClass);
+linkExternos.addEventListener('click', handleNoLink);
 
 // Selecione todos os elementos do site começando a partir do body,
 // ao clique mostre exatamente quais elementos estão sendo clicados
 
+const body = document.querySelector ('body');
+const mostrarBody = (e => { console.log(e.target)}); body.addEventListener ('click', mostrarBody);
+    
+  
+
 
 // Utilizando o código anterior, ao invés de mostrar no console,
 // remova o elemento que está sendo clicado, o método remove() remove um elemento
-
+const remover = (e)
 
 // Se o usuário clicar na tecla (t), aumente todo o texto do site. 
