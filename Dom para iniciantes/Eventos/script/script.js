8cc9b10cfcd7333c53e00fe2e0230a5a3d9a3d47
skunbydev <skunbydev@gmail.com>
2023-07-09 15:22:27 -0300

    novo exercicio

--- Dom para iniciantes/Eventos/script/script.js
+++ Dom para iniciantes/Eventos/script/script.js
@@ -2,13 +2,13 @@
 // adicione a classe ativo ao item clicado e remova dos
 // demais itens caso eles possuam a mesma. Previna
 // o comportamento padrão desses links
-
 const linkExternos = document.querySelector('a[href^="https"]');
 console.log(linkExternos);
 
 function handleNoLink(event) {
     event.preventDefault();
 }
+
 function handleAtivaClass(event) {
     if (linkExternos.className === 'ativo') {
         linkExternos.classList.remove('ativo');
@@ -18,18 +18,22 @@ function handleAtivaClass(event) {
 }
 linkExternos.addEventListener('click', handleAtivaClass);
 linkExternos.addEventListener('click', handleNoLink);
-
 // Selecione todos os elementos do site começando a partir do body,
 // ao clique mostre exatamente quais elementos estão sendo clicados
-
-const body = document.querySelector ('body');
-const mostrarBody = (e => { console.log(e.target)}); body.addEventListener ('click', mostrarBody);
-    
-  
-
-
+const body = document.querySelector('body');
+const mostrarBody = (e => {
+    console.log(e.target);
+});
 // Utilizando o código anterior, ao invés de mostrar no console,
 // remova o elemento que está sendo clicado, o método remove() remove um elemento
-const remover = (e)
+const removerBody = (e => `${e.target.remove()} ${console.log ('removido')}`);
+body.addEventListener('click', mostrarBody);
+body.addEventListener('click', removerBody);
 
-// Se o usuário clicar na tecla (t), aumente todo o texto do site. 
+// Se o usuário clicar na tecla (t), aumente todo o texto do site.
+const handleKeyboard = (e => {
+    if (e.key === 't') document.body.classList.toggle('text-big');
+});
+
+
+window.addEventListener('keydown', handleKeyboard);
