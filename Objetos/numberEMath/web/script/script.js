8640c7e5a0d78594b8b86a5b8666c0406b6dc124
skunbydev <skunbydev@gmail.com>
2023-07-18 18:07:04 -0300

    exer

--- Objetos/numberEMath/web/script/script.js
+++ Objetos/numberEMath/web/script/script.js
@@ -6,9 +6,30 @@ console.log (random);
 // Retorne o maior número da lista abaixo
 const numeros = '4, 5, 20, 8, 9';
 
+const arrayNumeros = numeros.split (',');
+const numeroMaximo = Math.max(...arrayNumeros);
+
+console.log (numeroMaximo);
 
 // Crie uma função para limpar os preços
 // e retornar os números com centavos arredondados
 // depois retorne a soma total
 const listaPrecos = ['R$ 59,99', ' R$ 100,222',
-                     'R$ 230  ', 'r$  200'];
+                     'R$ 230  ', 'r$  200']
+
+
+
+
+function limparPreco (preco) {
+    preco =  +preco.toUpperCase().replace('R$', '').trim().replace(',', '.');
+    preco = +preco.toFixed (2);
+    return preco;
+}
+
+let somaPreco = 0;
+
+listaPrecos.forEach (preco => {
+    somaPreco += limparPreco(preco)
+});
+
+console.log (somaPreco.toLocaleString('en-US', {style: 'currency', currency: 'BRL'}));
