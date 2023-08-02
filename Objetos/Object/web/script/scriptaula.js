ccff6397168ef4406b02cb0016d685e8f716eebe
skunbydev <skunbydev@gmail.com>
2023-08-02 17:58:05 -0300

    adicao novo exer

--- Objetos/Object/web/script/scriptaula.js
+++ Objetos/Object/web/script/scriptaula.js
@@ -5,15 +5,45 @@ const pessoa = ({
 
 console.log (pessoa);
 
-const carro = {
-    marca: 'Marca',
-    rodas: 4,
-    acelerar() {
-        return `${this.marca} acelerou`
+const carros = {
+    marca: 'Honda',
+    ano: 2023, 
+    init (valor) {
+    this.marca = valor; //definirá o valor, como marca. Fazendo o JS imprimir string.
+    return this;
     },
-    buzinar () {
-        return `${this.marca} buzinou`
+    buzinar() {
+    return `O carro da ${this.marca}, buzinou.`        
+    },
+    acelerou(velocidade) {
+     return `O carra da ${this.marca}, acelerou à ${velocidade}.`;   
     }
 }
 
-const honda = Object.create(carro);
+const honda = Object.create(carros).init('Honda');
+const ferrari = Object.create(carros).init('Ferrari');
+
+console.log (ferrari.acelerou('150km'));
+// honda.marca = 'Honda';
+// honda.acelerou();
+
+const funcaoAutomovel = {
+    acelerar () {
+        return 'acelerou';
+    },
+    buzinar () {
+        return 'buzinou';
+    }
+}
+
+const moto = {
+    rodas: 2,
+    capacete: true, 
+}
+const carro = {
+    rodas: 4,
+    mala: true,
+}
+
+Object.assign(moto, funcaoAutomovel);
+Object.assign(carro, funcaoAutomovel);
--- Objetos/Prototype/web/script/script.js
+++ Objetos/Prototype/web/script/script.js
@@ -1,63 +1,61 @@
-// Transforme o objeto abaixo em uma Constructor Function
-function Pessoa(nomePessoa, idadePessoa) {
-    this.nome = nomePessoa;
-        this.idade = idadePessoa;
-    this.andar = function() {
-        console.log(nomePessoa + ' andou');
-    }
+// Crie uma função construtora de Pessoas
+// Deve conter nome, sobrenome e idade
+// Crie um método no protótipo que retorne
+// o nome completo da pessoa
+function Pessoa (nome, sobrenome, idade) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade + ` anos`;
+}
+Pessoa.prototype.nomeCompleto = function () {
+    return this.nome + ' ' + this.sobrenome;
 }
 
+const luiz = new Pessoa('Luiz Felipe', 'Santos de Lima Alves', 20);
+console.log (luiz);
+// Liste os métodos acessados por 
+// dados criados com NodeList,
+// HTMLCollection, Document
+
+//item(index)
+//length
+
+//item(index): Retorna o elemento HTML na posição index da coleção.
+// namedItem(name): Retorna o elemento HTML com o atributo name especificado (apenas para coleções com atributos name).
+// length: Retorna o número de elementos HTML na coleção.
+
+// getElementById(id): Retorna o elemento HTML com o atributo id especificado.
+// getElementsByClassName(className): Retorna uma coleção de elementos HTML que têm a classe especificada.
+// getElementsByTagName(tagName): Retorna uma coleção de elementos HTML que têm a tag especificada.
+// querySelector(selector): Retorna o primeiro elemento que corresponde ao seletor CSS especificado.
+// querySelectorAll(selector): Retorna todos os elementos que correspondem ao seletor CSS especificado em uma NodeList.
+
+// Liste os construtores dos dados abaixo
+const li = document.querySelector('li');
+
+li; //lista propeamente dita
+li.click;  // click
+li.innerText; // conteudo dentro da LI
+li.value; // valor dentro da li
+li.hidden; // esconder
+li.offsetLeft; // lado direito, distancia
+li.click(); //efeito de click, amigo
+
+// Qual o construtor do dado abaixo:
+li.hidden.constructor.name;
+//String bolleana
 
 
-// Crie 3 pessoas, João - 20 anos,
-// Maria - 25 anos, Bruno - 15 anos
+// Exemplo de uso real, de um prototype.
 
-function Pessoas(nomeDaPessoa, idadeDaPessoa) {
-    this.nome = nomeDaPessoa;
-    this.idade = idadeDaPessoa;
-
-    this.dadosDaPessoa = function () {
-        return `${nomeDaPessoa} têm ${idadeDaPessoa} anos`;
-    };
+function Animal (nome) {
+    this.nome = nome;
 }
 
-
-const joao = new Pessoas ('Joao', 19);
-console.log (joao.dadosDaPessoa());
-
-const maria = new Pessoas ('Maria', 25);
-console.log (maria.dadosDaPessoa());
-
-const bruno = new Pessoas ('Bruno', 15);
-console.log (bruno.dadosDaPessoa());
-
-
-
-// Crie uma Constructor Function (Dom) para manipulação
-// de listas de elementos do dom. Deve conter as seguintes
-// propriedades e métodos:
-//
-// elements, retorna NodeList com os elementos selecionados
-// addClass(classe), adiciona a classe a todos os elementos
-// removeClass(classe), remove a classe a todos os elementos
-
-function Dom(seletor) {
-    const elementList = document.querySelectorAll(seletor);
-    this.elements = elementList;
-    this.addClass = function (classe) {
-        elementList.forEach (element => {
-            element.classList.add(classe);
-        });
-    this.removeClass = function (classe) {
-        elementList.forEach (element => {
-            element.classList.remove(classe)
-        });
-    }   
-    }
+Animal.prototype.gritar = function () {
+    return `A ${this.nome}, está gritando e fazendo escândalo.`
 }
 
+const cadela = new Animal ('Belinha');
+console.log (cadela.gritar());
 
-
-
-const listaItens = new Dom('li');
-listaItens.addClass('ativar');
