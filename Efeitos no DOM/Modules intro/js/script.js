a9d2273fb0db0cdf94f96dd9c0433dede324ad7b
skunbydev <skunbydev@gmail.com>
2023-08-04 16:58:56 -0300

    fix

---
+++ Efeitos no DOM/Modules intro/js/script.js
@@ -0,0 +1,8 @@
+import initScrollSuave from './js/modules/scroll-suave';
+import initAnimacaoScroll from './js/modules/animacao-scroll';
+import initAccordion from './js/modules/accordion';
+import initTabNav from './js/modules/TabNav';
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
