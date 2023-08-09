61913ef90428f4ae4bb2a723415ea96a7a846647
skunbydev <skunbydev@gmail.com>
2023-08-09 17:53:43 -0300

    novo exerc

--- Efeitos-no-DOM/DROPDOWN-MENU/js/modules/dropdown-menu.js
+++ Efeitos-no-DOM/DROPDOWN-MENU/js/modules/dropdown-menu.js
@@ -1,16 +1,21 @@
+import outsideClick from "./outsideclick.js";
+
 export default function initDropdownMenu() {
- 
+
     const dropdownMenu = document.querySelectorAll('[data-dropdown]')
 
     dropdownMenu.forEach(menu => {
-    ['click', 'touchstart'].forEach(userEvent => {
-        menu.addEventListener(userEvent, handleClick);
-    })
+        ['click', 'touchstart'].forEach(userEvent => {
+            menu.addEventListener(userEvent, handleClick);
+        })
     })
 
     function handleClick(event) {
         event.preventDefault();
         this.classList.toggle('active');
+        outsideClick(this, ['click', 'touchstart'], () => {
+            this.classList.remove('active');
+        });
 
     }
 }
