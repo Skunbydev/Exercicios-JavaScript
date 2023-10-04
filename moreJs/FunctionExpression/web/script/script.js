bea674452025844fe2e1b4c8b6cacad78c916791
skunbydev <skunbydev@gmail.com>
2023-10-04 17:57:04 -0300

    add exercise

---
+++ moreJs/FunctionExpression/web/script/script.js
@@ -0,0 +1,39 @@
+/*
+function somar (a, b) {
+    return a + b;
+}
+
+
+const somar = function (a, b) {
+    return a + b;
+}
+*/
+
+const somar = (a, b) => a + b;
+somar (4, 2);
+
+const quadrado = (a) => a *= a;
+console.log (quadrado(3));
+
+
+const instrumento = 'Guitarra';
+
+/*
+function initCodigo(){
+ const instrumento = 'Violão';
+ console.log(instrumento);
+}
+
+initCodigo();
+
+(function(){
+    const instrumento = "Violão";
+    console.log(instrumento);
+})();
+
+*/ 
+
+(()=>{
+    const instrumento = 'violão';
+    console.log(instrumento);
+})();
